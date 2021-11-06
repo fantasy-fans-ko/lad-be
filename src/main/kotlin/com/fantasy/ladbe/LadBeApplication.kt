b5314d4ce1d64546184ad00e75cb06aaf00035ec
juyohan <>
2021-11-06 15:48:23 +0900

    first commit

---
+++ src/main/kotlin/com/fantasy/ladbe/LadBeApplication.kt
@@ -0,0 +1,11 @@
+package com.fantasy.ladbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LadBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<LadBeApplication>(*args)
+}
---
+++ src/test/kotlin/com/fantasy/ladbe/LadBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fantasy.ladbe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LadBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
